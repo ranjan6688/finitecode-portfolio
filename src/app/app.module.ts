a7ebca59ada09b79d3dbfb3af92adde6266a382e
biswaranjan.nayak <biswaranjan@visnet.in>
2024-11-16 14:36:02 +0530

    v111624: All items placed in index page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExpertiseComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    TestimonialComponent,
+    PricingComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/expertise/expertise.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expertise',
+  templateUrl: './expertise.component.html',
+  styleUrl: './expertise.component.css'
+})
+export class ExpertiseComponent {
+
+}
